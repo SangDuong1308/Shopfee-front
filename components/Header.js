ebec247f406d60b2d5e9593cebd3d85b3a49d383
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2023-08-30 15:03:14 +0700

    dashboard

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,69 @@
 import Link from "next/link";
+import styled from "styled-components";
+import Center from "./Center";
+
+
+const StyledHeader = styled.header`
+    background-color: #222; 
+`;
+
+const Logo = styled(Link)`
+    color:#fff;
+    text-decoration: none;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+
+const StyledNav = styled.nav`
+  ${props => props.mobileNavActive ? `
+    display: block;
+  ` : `
+    display: none;
+  `}
+  gap: 15px;
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 70px 20px 20px;
+  background-color: #222;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    position: static;
+    padding: 0;
+  }
+`;
+
+const NavLink = styled(Link)`
+  display: block;
+  color:#aaa;
+  text-decoration:none;
+  padding: 10px 0;
+  @media screen and (min-width: 768px) {
+    padding:0;
+  }
+`;
 
 export default function Header() {
     return (
-        <header>
-            <Link href={'/'}>Shopfee</Link>
-            <nav>
-                <Link href={'/'}>Home</Link>
-                <Link href={'/products'}>All products</Link>
-                <Link href={'/categories'}>Categories</Link>
-                <Link href={'/account'}>Account</Link>
-                <Link href={'/cart'}>Cart</Link>
-            </nav>
-        </header>
+        <StyledHeader>
+            <Center>
+                <Wrapper>
+                    <Logo href={'/'}>Shopfee</Logo>
+                    <StyledNav>
+                        <NavLink href={'/'}>Home</NavLink>
+                        <NavLink href={'/products'}>All products</NavLink>
+                        <NavLink href={'/categories'}>Categories</NavLink>
+                        <NavLink href={'/account'}>Account</NavLink>
+                        <NavLink href={'/cart'}>Cart</NavLink>
+                    </StyledNav>
+                </Wrapper>
+            </Center>
+        </StyledHeader>
     );
 }
