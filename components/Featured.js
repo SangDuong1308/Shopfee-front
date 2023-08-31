ef93aa747e097b1ac7f173228db633d7082eb0ea
Duong Phuoc Sang <duongphuocsang1308@gmail.com>
2023-08-31 16:44:15 +0700

    Home Page

--- components/Featured.js
+++ components/Featured.js
@@ -1,9 +1,91 @@
+import { styled } from "styled-components"
+import Center from "./Center";
+import Button from "./Button";
+
+const Bg = styled.div`
+  background-color: #222;
+  color:#fff;
+  padding: 50px 0;
+`;
+const Title = styled.h1`
+  margin:0;
+  font-weight:normal;
+`;
+const Desc = styled.p`
+  color:#aaa;
+  font-size:.8rem;
+`;
+
+const ColumnsWrapper = styled.div`
+    display: grid;
+    grid-template-columns: .9fr 1.1fr;
+    gap: 40px;
+    img{
+        max-width: 100%;
+        max-height: 200px;
+        display: block;
+        margin: 0 auto;
+  }
+`;
+
+const Column = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 25px;
+`;
 
 export default function Featured({}) {
     return (
-        <div>
-            <h1></h1>
-            <p></p>
-        </div>
-    )
+      <Bg>
+        <Center>
+          <ColumnsWrapper>
+            <Column>
+              <div>
+                <Title>Meet our new products!</Title>
+                <Desc>
+                  Try one of our signature selections and see what everyone’s
+                  talking about or select ‘All Products’ at the top of the page
+                  to view all the possibilities.
+                </Desc>
+                <ButtonsWrapper>
+                  <Button white outline>
+                    Read more
+                  </Button>
+                  <Button primary>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path d="M2.25 2.25a.75.75 0 000 1.5h1.386c.17 0 .318.114.362.278l2.558 9.592a3.752 3.752 0 00-2.806 3.63c0 .414.336.75.75.75h15.75a.75.75 0 000-1.5H5.378A2.25 2.25 0 017.5 15h11.218a.75.75 0 00.674-.421 60.358 60.358 0 002.96-7.228.75.75 0 00-.525-.965A60.864 60.864 0 005.68 4.509l-.232-.867A1.875 1.875 0 003.636 2.25H2.25zM3.75 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zM16.5 20.25a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z" />
+                    </svg>
+                    Add to cart
+                  </Button>
+                </ButtonsWrapper>
+              </div>
+            </Column>
+            <Column>
+              <img src="https://ecommerce-proj-hcmus.s3.amazonaws.com/1693448695344.png"></img>
+            </Column>
+            {/* <Column>
+                        <div>
+                        <Title>Meet our new products!</Title>
+                            <Desc>Try one of our signature selections and see what everyone’s 
+                            talking about or select ‘All Products’ 
+                            at the top of the page to view 
+                            all the possibilities.</Desc>
+                            <Button white outline size="l">Read more</Button>
+                            <Button primary size="l">Add to cart</Button>
+                        </div>
+                    </Column> */}
+          </ColumnsWrapper>
+        </Center>
+      </Bg>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div>
       <Header />
-      <Featured/>
+      <Featured />
     </div>
   )
 }
